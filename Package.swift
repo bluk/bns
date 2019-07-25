8658e7d434a30661b74bcb827aaaf9cc1ff3da61
Bryant Luk <bryant.luk@bryantluk.com>
2019-07-24 19:45:59 -0500

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -10,11 +10,11 @@ public let package = Package(
         .library(name: "BNS", targets: ["BNS"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.4.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.1.1"),
-        .package(url: "https://github.com/apple/swift-nio-http2.git", from: "1.3.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.1.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.1.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.5.1")),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.2.0")),
+        .package(url: "https://github.com/apple/swift-nio-http2.git", .upToNextMajor(from: "1.4.0")),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", .upToNextMajor(from: "1.2.0")),
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/apple/swift-package-manager", .branch("swift-5.0-branch")),
     ],
     targets: [
